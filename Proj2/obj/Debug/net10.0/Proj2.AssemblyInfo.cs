efa2c624db1c55cb36c31b21682cc224f6ee7ed9
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-08-01 11:22:08 +0545

    complete crud

--- Proj2/obj/Debug/net10.0/Proj2.AssemblyInfo.cs
+++ Proj2/obj/Debug/net10.0/Proj2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proj2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4aa74927abdf0d73bf333f6377ac30a6a4e4078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36785ddf5a63ac2833d1d0f287e1761d5a61e6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proj2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proj2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
